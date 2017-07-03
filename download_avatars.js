2c1fa6b2aa6a47c92be0db86edabdfddca2b2ef0
jayl214 <jay.lin@mail.mcgill.ca>
2017-07-03 20:53:32 +0000

    DONE i think idk

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,32 @@
 var request = require('request');
+var fs = require('fs');
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   request({url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'} }, function(error, response, body){
-  if(error) cb(error);
-  var bodyOBJ = JSON.parse(body)
-  for (i in bodyOBJ) cb(error, bodyOBJ[i].avatar_url );
+    if(error) cb(error);
+    var bodyOBJ = JSON.parse(body)
+    for (i in bodyOBJ) {
+      cb(error, bodyOBJ[i].avatar_url );
+      downloadImageByURL(bodyOBJ[i].avatar_url, `avatars/${i}.jpg`)
+    }
   });
+
+}
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function(err){
+            console.log('error:',err)
+         })
+         .on('end', function(){
+            console.log('Download complete!')
+         })
+         .pipe(fs.createWriteStream(filePath));
+
 
 }
 
@@ -17,3 +35,5 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
