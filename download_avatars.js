7b825baf3fb447ec46031bbab1d08d026d7171a2
jayl214 <jay.lin@mail.mcgill.ca>
2017-07-03 21:12:09 +0000

    Accepts arguments from terminal

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var fs = require('fs');
+var args = process.argv.slice(2);
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -31,9 +32,7 @@ function downloadImageByURL(url, filePath) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors( args[0], args[1], function(err, result) {
+  console.log((args.length === 2  ? 'Errors: ' + err + '\nResult: ' + result : 'Please enter both the Owner and Name of the repo, in that order.'));
 });
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
