50309dcaa99177219c415841de801c669ff581bb
jayl214 <jay.lin@mail.mcgill.ca>
2017-07-03 20:16:00 +0000

    prints html of page

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  request({url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'} }, function(error, response, body){
+  if(error) cb(error);
+  cb(error, body);
+  });
+
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
